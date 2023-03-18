0df7d12c75cece442796657202d318ddf7f4c0c2
jpb <jakub.baranski.33@gmail.com>
2023-03-18 11:02:56 +0000

    Allow download + install of APKs from the app store
    Signed-off-by: jpb <jakub.baranski.33@gmail.com>

--- app/src/main/java/com/jpb/android/appget/STActivity2.java
+++ app/src/main/java/com/jpb/android/appget/STActivity2.java
@@ -2,13 +2,24 @@ package com.jpb.android.appget;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -17,9 +28,11 @@ import java.net.URL;
 public class STActivity2 extends AppCompatActivity {
 
     String TextFileURL = "https://github.com/jpbandroid/AppGet-Resources/raw/main/USP/updated_date.txt" ;
+    String TextFileURL2 = "https://github.com/jpbandroid/AppGet-Resources/raw/main/USP/version.txt" ;
     TextView textView10;
-    Button button ;
+    TextView textView11;
     Toolbar toolbar;
+    Button installbut;
     URL url ;
     String TextHolder = "" , TextHolder2 = "";
     BufferedReader bufferReader ;
@@ -27,12 +40,50 @@ public class STActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stactivity2);
-        textView10 = this.findViewById(R.id.textView11);
+        textView10 = this.findViewById(R.id.textView10);
+        textView11 = this.findViewById(R.id.textView11);
         toolbar = this.findViewById(R.id.toolbar);
+        installbut = this.findViewById(R.id.button2);
+        installbut.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                DownloadManager downloadmanager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri = Uri.parse("https://github.com/jpbandroid/AppGet-Resources/raw/main/USP/app-release.apk");
+
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setTitle("Unified ScratchTappy Platform");
+                request.setDescription("Downloading");
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "USP.apk");
+                downloadmanager.enqueue(request);
+                try {
+
+                    File outputFile = new File(Environment.DIRECTORY_DOWNLOADS, "USP.apk");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        Uri apkUri = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID + ".provider", outputFile);
+                        Intent intent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
+                        intent.setData(apkUri);
+                        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        getApplicationContext().startActivity(intent);
+                    } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N){
+                        Uri apkUri = Uri.fromFile(outputFile);
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        getApplicationContext().startActivity(intent);
+                    }else {
+                        Toast.makeText(getApplicationContext(), "File not found.", Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            };
+        });
         setSupportActionBar(toolbar);
         new GetUpdateDate().execute();
+
     }
 
+    @SuppressLint("StaticFieldLeak")
     public class GetUpdateDate extends AsyncTask<Void, Void, Void> {
 
         @Override
@@ -70,7 +121,51 @@ public class STActivity2 extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void finalTextHolder) {
 
-            textView10.setText("Updated: " + TextHolder);
+            textView11.setText("Updated: " + TextHolder);
+
+            super.onPostExecute(finalTextHolder);
+        }
+
+    }
+    @SuppressLint("StaticFieldLeak")
+    public class GetLatestVersion extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                url = new URL(TextFileURL2);
+
+                bufferReader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                while ((TextHolder2 = bufferReader.readLine()) != null) {
+
+                    TextHolder += TextHolder2;
+                }
+                bufferReader.close();
+
+            } catch (MalformedURLException malformedURLException) {
+
+                // TODO Auto-generated catch block
+                malformedURLException.printStackTrace();
+                TextHolder = malformedURLException.toString();
+
+            } catch (IOException iOException) {
+
+                // TODO Auto-generated catch block
+                iOException.printStackTrace();
+
+                TextHolder = iOException.toString();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void finalTextHolder) {
+
+            textView10.setText("Version: " + TextHolder);
 
             super.onPostExecute(finalTextHolder);
         }
