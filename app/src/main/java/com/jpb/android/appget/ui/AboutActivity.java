deb5654bf6d544aa5c61cb2fbf6046db54b8ed05
jpb <jakub.baranski.33@gmail.com>
2023-10-19 09:59:08 +0100

    Introduce AboutActivity
    Signed-off-by: jpb <jakub.baranski.33@gmail.com>

---
+++ app/src/main/java/com/jpb/android/appget/ui/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.jpb.android.appget.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jpb.android.appget.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
