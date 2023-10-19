ae92c0bf9894df0a2848008ebdf15358f1b0a5e1
jpb <jakub.baranski.33@gmail.com>
2023-10-19 10:02:56 +0100

    Make Account cards redirect to Activities, fixes
    Signed-off-by: jpb <jakub.baranski.33@gmail.com>

--- app/src/main/java/com/jpb/android/appget/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/jpb/android/appget/ui/notifications/NotificationsFragment.kt
@@ -1,5 +1,6 @@
 package com.jpb.android.appget.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.card.MaterialCardView
+import com.jpb.android.appget.STActivity2
+import com.jpb.android.appget.UpdateActivity
 import com.jpb.android.appget.databinding.FragmentNotificationsBinding
+import com.jpb.android.appget.ui.AboutActivity
 
 class NotificationsFragment : Fragment() {
 
@@ -27,6 +32,17 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val button: MaterialCardView = binding.updatebutton
+        button.setOnClickListener {
+            val intent = Intent(this@NotificationsFragment.requireContext(), UpdateActivity::class.java)
+            startActivity(intent)
+        }
+        val button2: MaterialCardView = binding.aboutbutton
+        button.setOnClickListener {
+            val intent = Intent(this@NotificationsFragment.requireContext(), AboutActivity::class.java)
+            startActivity(intent)
+        }
 
         //val textView: TextView = binding.textNotifications
         //notificationsViewModel.text.observe(viewLifecycleOwner) {
