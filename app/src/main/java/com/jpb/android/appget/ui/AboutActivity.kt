cfd8c80824fcd9943b2669166d978634e846095d
jpb <jakub.baranski.33@gmail.com>
2024-06-16 13:00:00 +0100

    Remove deprecated UIs

--- app/src/main/java/com/jpb/android/appget/ui/AboutActivity.kt
+++ app/src/main/java/com/jpb/android/appget/ui/AboutActivity.kt
@@ -2,19 +2,9 @@ package com.jpb.android.appget.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import com.jpb.android.appget.R
-import com.jpb.android.appget.SDKActivity
 import com.jpb.android.appget.oss.license.OSSLicense
 
 class AboutActivity : AppCompatActivity() {
