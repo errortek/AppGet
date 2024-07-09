e6d228c57c77daaf8af64203dac84d2d0b703f38
jpb <jakub.baranski.33@gmail.com>
2024-07-09 18:13:41 +0100

    Progress on app update functionality

--- app/src/main/java/com/jpb/android/appget/UpdateActivity.java
+++ app/src/main/java/com/jpb/android/appget/UpdateActivity.java
@@ -1,9 +1,15 @@
 package com.jpb.android.appget;
 
+import static android.view.View.GONE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -26,20 +32,34 @@ public class UpdateActivity extends AppCompatActivity {
         readVersion();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
-        MaterialCardView cardView = findViewById(R.id.cardU);
-        PackageManager packageManager = getPackageManager();
-        PackageInfo packageInfo = null;
-//        try {
-//            packageInfo = packageManager.getPackageInfo("com.jpb.scratchtappy", 0);
-//        } catch (PackageManager.NameNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
 
-        // Retrieve the version information
-        String versionName = "10";
-        int versionCode = 10000;
-        if (versionCode == onlineVersionCode) {
-            cardView.setVisibility(View.GONE);
+        MaterialCardView cardView = findViewById(R.id.cardU);
+
+        String your_apppackagename = "com.jpb.scratchtappy";
+        PackageManager packageManager = getPackageManager();
+        ApplicationInfo applicationInfo = null;
+        int versionCode;
+        versionCode = 1;
+        try {
+            applicationInfo = packageManager.getApplicationInfo(your_apppackagename, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (applicationInfo == null) {
+            // not installed it will open your app directly on playstore
+            versionCode = 1;
+        } else {
+            // Installed
+            PackageInfo AppInfo = null;
+            try {
+                AppInfo = packageManager.getPackageInfo(your_apppackagename, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+                //throw new RuntimeException(e);
+            }
+            versionCode = AppInfo.versionCode;
+            if (versionCode > onlineVersionCode) {
+                cardView.setVisibility(GONE);
+            }
         }
     }
     private void readVersion() {
@@ -80,6 +100,8 @@ public class UpdateActivity extends AppCompatActivity {
 
                             } catch (PackageManager.NameNotFoundException e) {
                                 e.printStackTrace();
+                                TextView text = (TextView) findViewById(R.id.textView24);
+                                text.setText( "e ->" + bo.toString());
                             }
                             try {
                                 bo.close();
@@ -94,5 +116,16 @@ public class UpdateActivity extends AppCompatActivity {
                 }
             }
         }.start();
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        return false;
     }
 }
