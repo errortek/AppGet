e866943e256d060a2e16d8132be55f9a9dc84f4e
jpb <jakub.baranski.33@gmail.com>
2024-07-12 08:57:11 +0100

    Implement more update functions

--- app/src/main/java/com/jpb/android/appget/UpdateActivity.kt
+++ app/src/main/java/com/jpb/android/appget/UpdateActivity.kt
@@ -15,41 +15,28 @@ import java.net.URL
 
 class UpdateActivity : AppCompatActivity() {
     private var onlineVersionCode = 0
+    var versionCode = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         readVersion()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update)
-
-        val cardView = findViewById<MaterialCardView>(R.id.cardU)
-
-        val your_apppackagename = "com.jpb.scratchtappy"
-        val packageManager = packageManager
-        var applicationInfo: ApplicationInfo? = null
-        var versionCode: Int
-        versionCode = 1
         try {
-            applicationInfo = packageManager.getApplicationInfo(your_apppackagename, 0)
+            val packageInfo =
+                packageManager.getPackageInfo("com.jpb.scratchtappy", 0)
+
+            // Retrieve the version information
+            versionCode = packageInfo.versionCode
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
-        if (applicationInfo == null) {
-            // not installed it will open your app directly on playstore
-            versionCode = 1
-        } else {
-            // Installed
-            var AppInfo: PackageInfo? = null
-            try {
-                AppInfo = packageManager.getPackageInfo(your_apppackagename, 0)
-            } catch (e: PackageManager.NameNotFoundException) {
-                //throw new RuntimeException(e);
-            }
-            versionCode = AppInfo!!.versionCode
-            if (versionCode > onlineVersionCode) {
+        val cardView = findViewById<MaterialCardView>(R.id.cardU)
+            if (versionCode < onlineVersionCode) {
                 cardView.visibility = View.GONE
             }
         }
-    }
+
 
     private fun readVersion() {
         object : Thread() {
@@ -67,46 +54,35 @@ class UpdateActivity : AppCompatActivity() {
                     val buffer = ByteArray(1024)
                     `in`.read(buffer) // Read from Buffer.
                     bo.write(buffer) // Write Into Buffer.
-                    runOnUiThread { // Get the package manager instance
-                        val packageManager = packageManager
+                        runOnUiThread { // Get the package manager instance
+                            val packageManager = packageManager
 
-                        try {
-                            // Get the package information
-                            val packageInfo =
-                                packageManager.getPackageInfo("com.jpb.scratchtappy", 0)
+                            try {
+                                // Get the package information
+                                val packageInfo =
+                                    packageManager.getPackageInfo("com.jpb.scratchtappy", 0)
 
-                            // Retrieve the version information
-                            val versionName = packageInfo.versionName
-                            val versionCode = packageInfo.versionCode
-                            val text = findViewById<View>(R.id.textView24) as TextView
-                            text.text = "$versionName->$bo"
-                            onlineVersionCode = bo.size()
-                        } catch (e: PackageManager.NameNotFoundException) {
-                            e.printStackTrace()
-                            val text = findViewById<View>(R.id.textView24) as TextView
-                            text.text = "e ->$bo"
+                                // Retrieve the version information
+                                val versionName = packageInfo.versionName
+                                val versionCode = packageInfo.versionCode
+                                val text = findViewById<View>(R.id.textView24) as TextView
+                                text.text = "$versionName->$bo"
+                                onlineVersionCode = bo.size()
+                            } catch (e: PackageManager.NameNotFoundException) {
+                                e.printStackTrace()
+                                val text = findViewById<View>(R.id.textView24) as TextView
+                                text.text = "e ->$bo"
+                            }
+                            try {
+                                bo.close()
+                            } catch (e: IOException) {
+                                e.printStackTrace()
+                            }
                         }
-                        try {
-                            bo.close()
-                        } catch (e: IOException) {
-                            e.printStackTrace()
-                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
                     }
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
             }
         }.start()
-    }
-
-    private fun appInstalledOrNot(uri: String): Boolean {
-        val pm = packageManager
-        try {
-            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES)
-            return true
-        } catch (e: PackageManager.NameNotFoundException) {
-        }
-
-        return false
     }
 }
