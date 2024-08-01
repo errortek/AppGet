dd67abf6a8755c767bdb8ce17b2fb5c5077ea54a
ErrorTek <jakub.baranski.33@gmail.com>
2024-08-01 17:41:18 +0100

    Remove Jetpack Compose due to compilation issues

--- app/src/main/java/com/jpb/android/appget/ui/AboutActivity.kt
+++ app/src/main/java/com/jpb/android/appget/ui/AboutActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.jpb.android.appget.R
-import com.jpb.android.appget.oss.license.OSSLicense
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +12,8 @@ class AboutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_about)
         val button3: Button = findViewById(R.id.button6)
         button3.setOnClickListener {
-            val intent = Intent(applicationContext, OSSLicense::class.java)
-            startActivity(intent)
+            //val intent = Intent(applicationContext, OSSLicense::class.java)
+            //startActivity(intent)
         }
     }
 }
