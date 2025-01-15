d1817a05c9f3c21368283edd015f7a4f4f6e68d0
ErrorTek <jakub.baranski.33@gmail.com>
2025-01-15 22:39:43 +0000

    Improve APK download flow

--- app/src/main/java/com/jpb/android/appget/AppDetailsActivity.kt
+++ app/src/main/java/com/jpb/android/appget/AppDetailsActivity.kt
@@ -1,13 +1,18 @@
 package com.jpb.appget.lite
 
+import android.annotation.SuppressLint
 import android.app.DownloadManager
+import android.database.Cursor
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.View
+import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,12 +20,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 
+
 class AppDetailsActivity : AppCompatActivity() {
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         val bundle = intent.extras
         val appName = bundle?.getString("App")
@@ -39,16 +49,77 @@ class AppDetailsActivity : AppCompatActivity() {
         val installbut = this.findViewById<Button>(R.id.InstallButton)
         val appTitleTextView = this.findViewById<TextView>(R.id.AppTitle)
         installbut.setOnClickListener(View.OnClickListener {
+            val appIconImageView = findViewById<ImageView>(R.id.AppIcon)
             val downloadmanager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
             val uri =
                 Uri.parse("https://github.com/jpbandroid/AppGet-Resources/raw/main/"+appName+"/app.apk")
+            val downloadProgress: CircularProgressIndicator = findViewById(R.id.download_progress)
+            downloadProgress.progress = 0
+            downloadProgress.visibility = View.VISIBLE
+            val thread = Thread {
+                try {
+                    var bitmap = fetchIcon(appName)
+                    appIconImageView.load(bitmap) {
+                            transformations(CircleCropTransformation())
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            thread.start()
 
+            installbut.text = "Cancel"
             val request = DownloadManager.Request(uri)
-            request.setTitle(appName)
+
             request.setDescription("Downloading")
+            request.setTitle(appName)
+            request.setMimeType("application/vnd.android.package-archive")
+            request.allowScanningByMediaScanner()
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, appName + ".apk")
-            downloadmanager.enqueue(request)
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "app.apk")
+
+            val manager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+
+            val downloadId = manager.enqueue(request)
+
+            Thread {
+                var downloading = true
+                while (downloading) {
+                    val q = DownloadManager.Query()
+                    q.setFilterById(downloadId)
+
+                    val cursor = manager.query(q)
+                    cursor.moveToFirst()
+                    val bytes_downloaded = cursor.getInt(
+                        cursor
+                            .getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR)
+                    )
+                    val bytes_total =
+                        cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+
+                    if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                        downloading = false
+                        runOnUiThread { downloadProgress.visibility = View.VISIBLE
+                            val thread = Thread {
+                                try {
+                                    var bitmap = fetchIcon(appName)
+                                    downloadProgress.visibility = View.GONE
+                                    appIconImageView.load(bitmap)
+                                    installbut.text = "Install"
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                }
+                            }.start()
+                        }
+                    }
+                    val dl_progress = ((bytes_downloaded * 100L) / bytes_total)
+
+                    runOnUiThread { downloadProgress.progress = dl_progress.toInt() }
+
+                    Log.d("DOWNLOAD", statusMessage(cursor))
+                    cursor.close()
+                }
+            }.start()
         })
         supportActionBar?.title = appName
         appTitleTextView.text = appName
@@ -142,5 +213,28 @@ class AppDetailsActivity : AppCompatActivity() {
                 }
             }
         }.start()
+    }
+
+    private fun fetchIcon(appName: String?): Bitmap? {
+        val path =
+            "https://github.com/jpbandroid/AppGet-Resources/raw/main/"+appName+"/icon.png"
+        val `in` = URL(path).openStream()
+        var bitmap = BitmapFactory.decodeStream(`in`)
+        return bitmap
+    }
+
+    @SuppressLint("Range")
+    private fun statusMessage(c: Cursor): String {
+
+        var msg: String = when (c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS))) {
+                DownloadManager.STATUS_FAILED -> "Download failed"
+                DownloadManager.STATUS_PAUSED -> "Download paused"
+                DownloadManager.STATUS_PENDING -> "Download pending"
+                DownloadManager.STATUS_RUNNING -> "Download in progress"
+                DownloadManager.STATUS_SUCCESSFUL -> "Download complete"
+                else -> "Download not found"
+            }
+
+        return (msg)
     }
 }
